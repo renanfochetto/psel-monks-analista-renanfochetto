6e9cbdbf4a89c3683e4d558c5f5dfa2b8dab9d80
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-04-21 23:07:49 -0300

    update fetch URL in useFooterCategories hook and format category data for improved handling

--- src/hooks/useSvgHook.js
+++ src/hooks/useSvgHook.js
@@ -8,32 +8,27 @@ const useSvgHook = () => {
   useEffect(() => {
     const fetchSvgs = async () => {
       try {
-        // Atualizando para o Live Link da API
-        const response = await fetch('https://fly-plume.localsite.io/wp-json/wp/v2/svgfooter');
+        const response = await fetch("https://psel-backend.shop/wp-json/acf/v3/svgfooter");
 
-        // Verifique se a resposta da API é válida
         if (!response.ok) {
-          throw new Error('Falha ao obter SVGs da API');
+          throw new Error("Falha ao obter SVGs da API");
         }
 
         const data = await response.json();
 
-        // Verifique se a resposta contém dados válidos
         if (!Array.isArray(data)) {
-          throw new Error('Dados retornados não são um array esperado');
+          throw new Error("Dados retornados não são um array");
         }
 
-        // Formata os dados dos SVGs para o front-end
         const formattedSvgs = data.map((item) => ({
           id: item.id,
-          name: item.acf?.name || "Nome não definido",
-          svg: item.acf?.svg || null,
-          link: item.link || "#",
+          name: item.acf?.name || "Sem nome",
+          svg: item.acf?.svg || "",
         }));
 
         setSvgs(formattedSvgs);
       } catch (err) {
-        setError(`Erro ao carregar SVGs: ${err.message || "Erro desconhecido"}`);
+        setError(`Erro ao carregar SVGs: ${err.message}`);
       } finally {
         setIsLoading(false);
       }
