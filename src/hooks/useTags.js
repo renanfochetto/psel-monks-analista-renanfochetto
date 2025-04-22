46cb4cf4d848b2aadec7e189be03e98610452894
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-04-21 22:25:22 -0300

    update fetch URL in useTags hook for improved API integration and add debugging logs

--- src/hooks/useTags.js
+++ src/hooks/useTags.js
@@ -9,7 +9,7 @@ const useTags = () => {
     const fetchTags = async () => {
       try {
         // Atualizando para o Live Link da API
-        const response = await fetch('https://fly-plume.localsite.io/wp-json/wp/v2/tagsection?per_page=100');
+        const response = await fetch('https://psel-backend.shop/wp-json/acf/v3/tagsection');
 
         // Verifica se a resposta da API foi bem-sucedida
         if (!response.ok) {
@@ -18,14 +18,18 @@ const useTags = () => {
 
         const data = await response.json();
 
-        // Valida se a estrutura dos dados está conforme esperado
+        // Depuração: logando os dados recebidos para verificar a estrutura
+        console.log('Dados recebidos:', data);
+
+        // Verifica se a estrutura dos dados está conforme esperado
         if (!Array.isArray(data)) {
           throw new Error('Os dados retornados não são um array');
         }
 
+        // Formatação e verificação dos dados de tags
         const formattedTags = data
-          .map(item => item.acf?.tag)
-          .filter(tag => tag)
+          .map(item => item.acf?.tag)   // Acessando de forma segura item.acf.tag
+          .filter(tag => tag)          // Filtra qualquer valor que seja undefined ou null
           .sort((a, b) => a.localeCompare(b)); // Ordena alfabeticamente
 
         setTags(formattedTags);
